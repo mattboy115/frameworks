ddc78c0c69ba14d89b40455f1be0f7a03412a946
Matt Lee <matthew_c_lee@yahoo.com>
2016-04-07 01:37:57 -0400

    jkljl

---
+++ Week1/Assignment/js/main.js
@@ -0,0 +1,16 @@
+var app = angular.module('myApp', []);
+
+app.controller('GroceryController', function($scope){
+
+    $scope.groceries = ['Nails', 'Pipe', 'Wood'];
+
+    $scope.addItem = function() {
+        $scope.groceries.push($scope.newItem);
+        $scope.newItem = '';
+    }
+
+    $scope.removeItem = function(item) {
+        var idx = $scope.groceries.indexOf(item);
+            $scope.groceries.splice(idx.l);
+    }
+});
